53dc7dd10a75bdd5453a11572c630bf32c38c96c
John Lagoutaris <jjlagoutaris@gmail.com>
2022-01-31 19:46:42 -0500

    I got the calculator into a working, albeit slightly clunkly state where everything has to flow through the equals button

--- calculator.js
+++ calculator.js
@@ -1,4 +1,6 @@
-const screen = document.querySelector('#calculator-screen'); 
+const screen = document.querySelector('#calculator-screen');
+let calculatorCache = [0];
+let operator;
 
 function add(a, b){
     return a + b;
@@ -13,7 +15,12 @@ function multiply(a, b){
 }
 
 function divide(a, b){
-    return a / b;
+    if(+b !== 0){
+        return a / b;
+    }
+    else{
+        alert("Ha!");
+    }
 }
 
 function operate(operator, a, b){
@@ -54,10 +61,42 @@ function addBtnListeners(){
                     break;
                 case "CLEAR":
                     screen.textContent = "";
+                    calculatorCache = [0];
                     break;
                 case "DELETE":
                     screen.textContent = screen.textContent.slice(0,-1);
-                    break;       
+                    break;
+                case "+":
+                    calculatorCache.push(+screen.textContent);
+                    screen.textContent = "";
+                    operator = '+';
+                    break;
+                case "-":
+                    calculatorCache.push(+screen.textContent);
+                    screen.textContent = "";
+                    operator = '-';
+                    break;
+                case "/":
+                    calculatorCache.push(+screen.textContent);
+                    screen.textContent = "";
+                    operator = '/';
+                    break;
+                case "*":
+                    calculatorCache.push(+screen.textContent);
+                    screen.textContent = "";
+                    operator = '*';
+                    break;
+                case "=":
+                    if (operator === '+' || operator === '-' || operator === '*' || operator === '/'){
+                        screen.textContent = +operate(operator, 
+                            calculatorCache[calculatorCache.length-1], 
+                            +screen.textContent).toFixed(5);
+                        operator = '';
+                    }
+                    else{
+                        screen.textContent = "Error, invalid operation";
+                    }
+                    break;
             }
         });
     });
